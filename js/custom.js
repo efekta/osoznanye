c23070be06ee814520ce8aa42ad7cdc22180fe48
Ptashkina Liliya <efekta9@gmail.com>
2019-02-22 17:52:32 +0300

    no message

--- js/custom.js
+++ js/custom.js
@@ -33,8 +33,8 @@
 	      settings: {
 	        slidesToShow: 1,
 	        slidesToScroll: 1,
-	        arrow: false,
-	        dots: true
+	        arrow: true,
+	        dots: false
 	      }
 	    },
 	    {
@@ -42,8 +42,8 @@
 	      settings: {
 	        slidesToShow: 1,
 	        slidesToScroll: 1,
-	        arrow: false,
-	        dots: true
+	        arrow: true,
+	        dots: false
 	      }
 	    },
 	    {
@@ -51,13 +51,50 @@
 	      settings: {
 	        slidesToShow: 1,
 	        slidesToScroll: 1,
-	        arrow: false,
-	        dots: true
+	        arrow: true,
+	        dots: false
 	      }
 	    }
 	  	]
 	});
+//     var dots = $('.carousel__item');
+// 	//вешаем обработчик на наши точки
+// 	dots.click(function(){
+// 		var $this = $(this);
+// 		dots.removeClass('before after');
+// 		//отображаем 2 предыдущие точки
+// 		$this
+// 			.prev().addClass('before')
+// 			.prev().addClass('before');
+// 		//отображаем 2 следующие точки
+// 		$this
+// 			.next().addClass('after')
+// 			.next().addClass('after');
 
+	
+// 	  //если мы в самом начале - добавляем пару последующих точек
+// 		if(!$this.prev().length) {
+// 	  	$this.next().next().next()
+// 		  	.addClass('after').next()
+// 			  .addClass('after');
+//   	}
+// 		//на 2й позиции - добавляем одну точку
+// 	  if(!$this.prev().prev().length) {
+//   		$this.next().next().next()
+// 	  	  .addClass('after');
+//   	}
+// 		//в самом конце - добавляем пару доп. предыдущих точек
+// 		if(!$this.next().length) {
+// 			$this.prev().prev().prev()
+// 				.addClass('before').prev()
+// 				.addClass('before');
+// 		}
+// 		//предпоследний элемента - добавляем одну пред. точку
+// 		if(!$this.next().next().length) {
+// 			$this.prev().prev().prev()
+// 				.addClass('before');
+// 		}	
+// });
 // mask input phone
 
   // $(".phone").mask("+7(999) 999-9999");

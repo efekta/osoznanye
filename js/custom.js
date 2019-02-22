51420344fecb8959a837fa330b94bef65b540526
Ptashkina Liliya <efekta9@gmail.com>
2019-02-22 17:05:12 +0300

    no message

--- js/custom.js
+++ js/custom.js
@@ -97,6 +97,17 @@
 	  midClick: true
 	});
 
+	// wow = new WOW(
+ //     {
+ //      boxClass:     'wow',      // default
+ //      animateClass: 'animated', // default
+ //      offset:       0,          // default
+ //      mobile:       true,       // default
+ //      live:         true        // default
+ //    }
+ //    )
+ //    wow.init();
+
 });
 
 

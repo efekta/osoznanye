4df5d94ac52ae650cb4518cf7fc4447cf55080e6
Ptashkina Liliya <efekta9@gmail.com>
2019-02-22 15:56:36 +0300

    no message

--- js/custom.js
+++ js/custom.js
@@ -70,10 +70,32 @@
 	}).attr('data-fancybox', 'group1');
 
 
-  	setTimeout(function() {
-  		animationEffect : 'fade'
-  		$('.modal').modal('show');}, 3000);
+	// Inline popups
+	$('.inline-popups').magnificPopup({
+	  delegate: 'a',
+	  removalDelay: 500, //delay removal by X to allow out-animation
+	  callbacks: {
+	    beforeOpen: function() {
+	       this.st.mainClass = this.st.el.attr('data-effect');
+	    }
+	  },
+	  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+	});
 
+	// Hinge effect popup
+	$('a.hinge').magnificPopup({
+	  mainClass: 'mfp-with-fade',
+	  removalDelay: 1000, //delay removal by X to allow out-animation
+	  callbacks: {
+	    beforeClose: function() {
+	        this.content.addClass('hinge');
+	    }, 
+	    close: function() {
+	        this.content.removeClass('hinge'); 
+	    }
+	  },
+	  midClick: true
+	});
 
 });
 

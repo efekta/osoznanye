21f9d5263721f7e36e9a25eda0adbbc8d743f61c
Ptashkina Liliya <efekta9@gmail.com>
2019-02-27 14:47:43 +0300

    no message

--- js/custom.js
+++ js/custom.js
@@ -2,14 +2,14 @@
 	/*========================
 	 fixed header
 	 =======================*/
-	// $(window).scroll(function() {
-	// 	if($(this).scrollTop() >= 250) {
-	// 	$('.page-head').addClass('page-head_fixed');
-	// }
-	// else{
-	// 	$('.page-head').removeClass('page-head_fixed');
-	// 	}
-	// });
+	$(window).scroll(function() {
+		if($(this).scrollTop() >= 250) {
+		$('.page-head').addClass('page-head_fixed');
+	}
+	else{
+		$('.page-head').removeClass('page-head_fixed');
+		}
+	});
 	/*========================
 	 mob menu
 	 =======================*/
